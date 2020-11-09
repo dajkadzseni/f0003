64234cb9f5dbe85b8b7b9bc0ae742c50d625df78
dajkadzseni <71691882+dajkadzseni@users.noreply.github.com>
2020-11-09 18:56:56 +0100

    Add files via upload

---
+++ f0003b.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+vezetéknév = input('Mi a vezetékneved? ')
+keresztnév = input('Mi a keresztneved? ')
+év = input('Melyik évben születtél? ')
+csillagjegy = input('Melyik csillagjegyben születtél? ')
+print('A te neved ', vezetéknév, ' ', keresztnév, ".", sep='')
+print('Az Úr ', év, '. évében születtél, a ', csillagjegy, ' jegyében.' , sep='')
